cf9e4931fc913fc44de956462f64d016a09e1b1b
sheesa azeeleeza <leezaluncheon@gmail.com>
2018-03-07 01:58:06 +0000

    automatically backed up by learn

--- fizzbuzz.rb
+++ fizzbuzz.rb
@@ -1,10 +1,10 @@
 def fizzbuzz(number)
-  if number % 3 == 0
-    return "Fizz"
+  if number % 5 == 0 && number % 3 ==0
+    return "FizzBuzz"
   elsif number % 5 == 0
     return "Buzz"
-  elsif number % 5 == 0 && number % 3 ==0
-    return "FizzBuzz"
+  elsif number % 3 == 0
+    return "Fizz"
   else
     return
   end
